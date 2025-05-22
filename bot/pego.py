3d2d28f9ed4fa2189d92f66d0c63823a85f862b7
bitbucket <panten@gmail.com>
2025-05-22 11:17:20 -0400

    Pego pickup search
    change view angle several times if cannot open pego inventory.

--- bot/pego.py
+++ bot/pego.py
@@ -25,7 +25,22 @@ def pego_pickup(metadata):
         utils.set_yaw(metadata.yaw)
         utils.turn_up(5) #Bitbucket Changed from 15 to 5 due to raised TP
         time.sleep(0.2*settings.sleep_constant)
-        ASA.strucutres.inventory.open()
+        if not ASA.strucutres.inventory.open():
+            logs.logger.debug(f"Searching for sunken pego at {metadata.name}")
+            utils.zero()
+            utils.set_yaw(metadata.yaw)
+            utils.turn_up(2) #Bitbucket Changed from 15 to 5 due to raised TP
+            utils.turn_left(10)
+            time.sleep(0.2*settings.sleep_constant)
+        if not ASA.strucutres.inventory.open():
+            logs.logger.debug(f"Searching for floating pego at {metadata.name}")
+            utils.zero()
+            utils.set_yaw(metadata.yaw)
+            utils.turn_up(10) #Bitbucket Changed from 15 to 5 due to raised TP
+            utils.turn_left(10)
+            time.sleep(0.2*settings.sleep_constant)
+            ASA.strucutres.inventory.open()
+        
         if attempt >= bot.config.pego_attempts:
             logs.logger.error(f"the pego at {metadata.name} could not be accesssed after {attempt} attempts")
             ASA.player.player_state.lastError = time.time()
